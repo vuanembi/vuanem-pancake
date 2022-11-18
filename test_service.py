34ef2896ce85ef5b1e6076d48248a25912c892fa
buithuhak22httta <realchanexok@gmail.com>
2022-11-18 11:56:08 +0700

    update test_service.py

--- test_service.py
+++ test_service.py
@@ -1,19 +1,38 @@
 import pytest
 import datetime
 import time
-from Pancake.get_Page_Statistics import getPageStatistics
+from pancake.get_statistics import snake_case
+from pancake.pipeline.pages import transform, schema
+from db.bigquery import load
+
 
 @pytest.fixture
 def since():
-    since=datetime.datetime.now() + datetime.timedelta(days = -5)
-    since = int(time.mktime(since.timetuple()))    
+    since = datetime.datetime.now() + datetime.timedelta(days=-5)
+    since = int(time.mktime(since.timetuple()))
     return since
+
 
 @pytest.fixture
 def until():
-    until=datetime.datetime.now()
+    until = datetime.datetime.now()
     until = int(time.mktime(until.timetuple()))
     return until
 
-def test_get_page_statistics(since,until):
-    getPageStatistics(since,until)
+
+@pytest.fixture
+def name():
+    name = "pages"
+    return name
+
+
+def test_get_statistics(since, until, name):
+    snake_case(since, until, name)
+
+
+def test_transform(since, until, name):
+    transform(snake_case(since, until, name))
+
+
+def test_load(since, until, name):
+    load(transform(snake_case(since, until, name)), schema(), name)
